a3735e956c6cca6913e21641909597a716a1acd6
Emmanuel <aboagyeapau@gmail.com>
2024-01-05 22:56:07 -0500

    minor update

--- conductivity-prediction-wastewater-treatment_ann.py
+++ conductivity-prediction-wastewater-treatment_ann.py
@@ -8,7 +8,7 @@
 #%% Import modules
 import numpy as np
 import pandas as pd
-import random
+import random, os
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, make_scorer
@@ -16,12 +16,19 @@ from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers.legacy import Adam
 from tensorflow.keras.layers import LeakyReLU
 from tensorflow.keras.initializers import GlorotUniform, GlorotNormal, HeNormal
 from hyperopt import hp, fmin, tpe, Trials, STATUS_OK, space_eval
 from hyperopt.pyll.base import scope
 from sklearn.inspection import permutation_importance
+
+#%% create a results folder
+results_folder = './wwt-conductivity-results'
+
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
 #%%
 # set random seed for reproducability
 SEED=0
@@ -66,16 +73,18 @@ def data_splitting(feature_set,label_set,ts=0.3,rs=42):
 
 # function for nueral network model
 def nn_model(feature_train):
-    tf.keras.utils.set_random_seed(SEED)
-    model = Sequential()
-    model.add(Dense(units=60, input_dim=feature_train.shape[1], activation='elu', kernel_initializer=GlorotUniform(seed=0)))
-    model.add(Dropout(rate=0.2))
-    model.add(Dense(units=40, kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=0.05)))
-    model.add(Dropout(rate=0.3))
-    model.add(Dense(units=20, kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=0.05)))
-    model.add(Dropout(rate=0.3))
-    model.add(Dense(units=1, activation='relu', kernel_initializer=GlorotUniform(seed=0)))
-    model.compile(loss='mean_squared_logarithmic_error', optimizer=Adam(learning_rate=0.01))
+    device_name = "/GPU:0" if tf.test.gpu_device_name() == "/device:GPU:0" else "/CPU:0"
+    with tf.device(device_name):
+        tf.keras.utils.set_random_seed(SEED)
+        model = Sequential()
+        model.add(Dense(units=60, input_dim=feature_train.shape[1], activation='elu', kernel_initializer=GlorotUniform(seed=0)))
+        model.add(Dropout(rate=0.2))
+        model.add(Dense(units=40, kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=0.05)))
+        model.add(Dropout(rate=0.3))
+        model.add(Dense(units=20, kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=0.05)))
+        model.add(Dropout(rate=0.3))
+        model.add(Dense(units=1, activation='relu', kernel_initializer=GlorotUniform(seed=0)))
+        model.compile(loss='mean_squared_logarithmic_error', optimizer=Adam(learning_rate=0.01))
     return model
 
 # function to make predictions 
@@ -98,7 +107,7 @@ def model_evaluation(label_train,label_train_pred,label_val,label_val_pred,label
     return [rmse_train, rmse_val, rmse_test, r2_score_train, r2_score_val, r2_score_test]
 
 # function for plotting
-def plot_results(label_train,label_train_pred,label_val,label_val_pred,label_test,label_test_pred,r2_score_train,r2_score_val,r2_score_test):
+def plot_results(label_train,label_train_pred,label_val,label_val_pred,label_test,label_test_pred,r2_score_train,r2_score_val,r2_score_test,figure_name,title='no title'):
     plt.figure(figsize=(7,5), dpi=700)
     plt.plot(label_train, label_train_pred, 'bo', label=f'train: r-sqaured={r2_score_train:.3f}')
     plt.plot(label_val, label_val_pred, 'yo', label=f'valid: r-sqaured={r2_score_val:.3f}')
@@ -106,12 +115,13 @@ def plot_results(label_train,label_train_pred,label_val,label_val_pred,label_tes
     plt.plot([label_train.min(), label_train.max()], [label_train.min(), label_train.max()], 'k--')
     plt.xlabel('Output Conductivity (Actual)')
     plt.ylabel('Output Conductivity, (Predicted)')
-    plt.title('Prediction of Conductivity for Wastewater Treatment Plant')
+    plt.title(title)
     plt.legend()
+    plt.savefig(f"./wwt-conductivity-results/{figure_name}.png")
     plt.show()
     
 #%% Import data
-path = 'water-treatment.data'
+path = './data/water-treatment.data'
 data = import_data(path)
 
 #%% Organze data into features and label
@@ -123,8 +133,10 @@ scaled_feature_set = feature_standardization(feature_set)
 feature_train, feature_val, feature_test, label_train, label_val, label_test = data_splitting(scaled_feature_set, label_set, ts=0.3, rs=42)
 
 #%% fit model 
-model = nn_model(feature_train)
-model.fit(feature_train, label_train, epochs=4000, batch_size=0, verbose=1, validation_data=(feature_val,label_val))
+device_name = "/GPU:0" if tf.test.gpu_device_name() == "/device:GPU:0" else "/CPU:0"
+with tf.device(device_name):
+    model = nn_model(feature_train)
+    model.fit(feature_train, label_train, epochs=4000, batch_size=0, verbose=1, validation_data=(feature_val,label_val))
 
 #%% make some predictions
 label_train_pred, label_val_pred, label_test_pred = make_predictions(model, feature_train, feature_val, feature_test)
@@ -133,7 +145,8 @@ label_train_pred, label_val_pred, label_test_pred = make_predictions(model, feat
 rmse_train,rmse_val,rmse_test,r2_score_train,r2_score_val,r2_score_test =  model_evaluation(label_train,label_train_pred,label_val,label_val_pred,label_test,label_test_pred)
 
 #%% plotting
-plot_results(label_train,label_train_pred,label_val,label_val_pred,label_test,label_test_pred,r2_score_train,r2_score_val,r2_score_test)
+
+plot_results(label_train,label_train_pred,label_val,label_val_pred,label_test,label_test_pred,r2_score_train,r2_score_val,r2_score_test,title='Without Hyperparameter Tuning (ANN)',figure_name='without-tuning-ann')
 
 #%% Use hyperopt to tune hyperparameters 
 # Define search space and function for hyperparameter tuning using 'hyperopt'
@@ -153,17 +166,18 @@ space = {
 
 # define an objective function
 def ann_model_hppt(space): 
-    tf.keras.utils.set_random_seed(SEED)
-    model = Sequential()
-    model.add(Dense(units=space['units'], input_dim=feature_train.shape[1], activation=space['activation'], kernel_initializer=GlorotUniform(seed=0)))
-    model.add(Dropout(rate=space['rate']))
-    model.add(Dense(units=space['units1'], kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=space['alpha'])))
-    model.add(Dropout(rate=space['rate1']))
-    model.add(Dense(units=space['units2'], kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=space['alpha'])))
-    model.add(Dropout(rate=space['rate1']))
-    model.add(Dense(units=1, activation='relu', kernel_initializer=GlorotUniform(seed=0)))
-    model.compile(loss=space['loss'], optimizer=Adam(learning_rate=space['learning_rate']))
-    model.fit(feature_train, label_train, epochs=space['epochs'], batch_size=space['batch_size'], verbose=0, validation_data=(feature_val,label_val))
+    with tf.device(device_name):
+        tf.keras.utils.set_random_seed(SEED)
+        model = Sequential()
+        model.add(Dense(units=space['units'], input_dim=feature_train.shape[1], activation=space['activation'], kernel_initializer=GlorotUniform(seed=0)))
+        model.add(Dropout(rate=space['rate']))
+        model.add(Dense(units=space['units1'], kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=space['alpha'])))
+        model.add(Dropout(rate=space['rate1']))
+        model.add(Dense(units=space['units2'], kernel_initializer=GlorotUniform(seed=0), activation=LeakyReLU(alpha=space['alpha'])))
+        model.add(Dropout(rate=space['rate1']))
+        model.add(Dense(units=1, activation='relu', kernel_initializer=GlorotUniform(seed=0)))
+        model.compile(loss=space['loss'], optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=space['learning_rate']))
+        model.fit(feature_train, label_train, epochs=space['epochs'], batch_size=space['batch_size'], verbose=0, validation_data=(feature_val,label_val))
     label_pred_val = model.predict(feature_val)
     label_pred_train = model.predict(feature_train)
     score = np.sqrt(mean_squared_error(label_val, label_pred_val)) + np.sqrt(mean_squared_error(label_train, label_pred_train)) * 0.5   # just a trick I use to improve my model 
@@ -206,8 +220,10 @@ def ann_model_tune(feature_train):
     model.compile(loss='logcosh', optimizer=Adam(learning_rate=0.0018902548925562176))
     return model
 
-model_hppt = ann_model_tune(feature_train)
-model_hppt.fit(feature_train, label_train, epochs=4000, batch_size=100, verbose=1, validation_data=(feature_val,label_val))
+with tf.device(device_name):
+    print(f"training on a {device_name}")
+    model_hppt = ann_model_tune(feature_train)
+    model_hppt.fit(feature_train, label_train, epochs=4000, batch_size=100, verbose=1, validation_data=(feature_val,label_val))
 
 #%% make some predictions
 label_train_pred_hppt, label_val_pred_hppt, label_test_pred_hppt = make_predictions(model_hppt, feature_train, feature_val, feature_test)
@@ -216,7 +232,7 @@ label_train_pred_hppt, label_val_pred_hppt, label_test_pred_hppt = make_predicti
 rmse_train_hppt,rmse_val_hppt,rmse_test_hppt,r2_score_train_hppt,r2_score_val_hppt,r2_score_test_hppt =  model_evaluation(label_train,label_train_pred_hppt,label_val,label_val_pred_hppt,label_test,label_test_pred_hppt)
 
 #%% plotting
-plot_results(label_train,label_train_pred_hppt,label_val,label_val_pred_hppt,label_test,label_test_pred_hppt,r2_score_train_hppt,r2_score_val_hppt,r2_score_test_hppt)
+plot_results(label_train,label_train_pred_hppt,label_val,label_val_pred_hppt,label_test,label_test_pred_hppt,r2_score_train_hppt,r2_score_val_hppt,r2_score_test_hppt,title='With Hyperparameter Tuning (ANN)',figure_name='with-tuning-ann')
 
 #%% Feature importance
 scorer = make_scorer(mean_squared_error, greater_is_better=False)
@@ -228,11 +244,8 @@ plt.figure(figsize=(10,7), dpi=700)
 plt.barh(range(len(idxx)),importances[idxx])
 plt.xlabel("Nerual Network Model: Feature Importance")
 plt.yticks(range(len(idxx)), [feature_set.columns[i] for i in idxx])
+plt.tight_layout()
+plt.savefig('./wwt-conductivity-results/feature-importance-ann.png')
 plt.show()
 
 #%%
-'''
-Ideally, you would want to tune your model first and then create the architechture from the optimized hyperparamters
-
-You can also refit the model using the, probably, top 5 features with the highest importances. 
-'''
